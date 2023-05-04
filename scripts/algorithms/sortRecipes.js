6c83f98db382b221a274eb6c36d113612a55461c
Baptiste LABAUNE <baptiste.labaune@gmail.com>
2023-05-04 17:33:47 +0200

    fonctionnal changes

--- scripts/algorithms/sortRecipes.js
+++ scripts/algorithms/sortRecipes.js
@@ -1,22 +1,20 @@
-export function sortByInput(data) {
-  const input = document.querySelector("#search-input").value.toLowerCase();
-  const sortedRecipes = [];
-  for (let i = 0; i < data.length; i++) {
-    const { ingredients, name, description } = data[i];
+//Fonction qui filtre les recettes comprenant l'input et les intègre dans un autre tableau
+export function sortByInput(input, data) {
+  let sortedRecipes = data.filter((recipe) => {
+    const { name, description, ingredients } = recipe;
     if (name.toLowerCase().includes(input)) {
-      sortedRecipes.push(data[i]);
-      continue;
+      return true;
     } else if (description.toLowerCase().includes(input)) {
-      sortedRecipes.push(data[i]);
-      continue;
+      return true;
+    } else if (
+      ingredients.some((ingredients) =>
+        ingredients.ingredient.toLowerCase().includes(input)
+      )
+    ) {
+      return true;
+    } else {
+      return false;
     }
-    for (let j = 0; j < ingredients.length; j++) {
-      const { ingredient } = ingredients[j];
-      if (ingredient.toLowerCase().includes(input)) {
-        sortedRecipes.push(data[i]);
-        break;
-      }
-    }
-  }
+  });
   return sortedRecipes;
 }
